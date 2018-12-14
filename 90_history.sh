7fbddbe75d83241b71ed9a4b3949d3b79c43253d
foxx1337 <foxx1337@yahoo.com>
2018-12-14 18:12:48 +0100

    Change PROMPT_COMMAND order to save git_bash_completion statuses

--- 90_history.sh
+++ 90_history.sh
@@ -9,7 +9,12 @@ shopt -s histappend
 export HISTSIZE=1000000
 export HISTFILESIZE=20000000
 
-export PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
+if [ -z $PROMPT_COMMAND ]
+then
+	export PROMPT_COMMAND="history -a"
+else
+	export PROMPT_COMMAND="$PROMPT_COMMAND;history -a"
+fi
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
